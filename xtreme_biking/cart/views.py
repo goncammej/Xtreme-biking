c0fd7199aac2c4d8ba5c97bec914a82e605cfb9c
alejandromd <aleprieto02@gmail.com>
2023-11-17 19:25:13 +0100

    feat: CART VIEW

--- xtreme_biking/cart/views.py
+++ xtreme_biking/cart/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.http import HttpResponseBadRequest
 
 # Create your views here.
+
+def cart_view(request):
+    cart_items = [
+        {'product': {'name': 'Producto 1', 'price': 10}, 'quantity': 2, 'total': 20},
+        {'product': {'name': 'Producto 2', 'price': 20}, 'quantity': 1, 'total': 20},
+        {'product': {'name': 'Producto 3', 'price': 30}, 'quantity': 3, 'total': 90},
+    ]
+    cart_total = 130
+    context = {
+        'cart_items': cart_items,
+        'cart_total': cart_total,
+    }
+    return render(request, 'cart.html', context)
--- xtreme_biking/xtreme_biking/settings.py
+++ xtreme_biking/xtreme_biking/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cart',
+    'client',
+    'order',
+    'payment',
+    'store',
 ]
 
 MIDDLEWARE = [
