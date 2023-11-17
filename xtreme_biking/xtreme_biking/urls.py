e5575ae7000a724beafa0ac81ee2cff305a71350
david <davdelpal1@alum.us.es>
2023-11-17 02:12:35 +0100

    Se han añadido las vistas de login y register, con los botones incluidos para pasar entre ellas

--- xtreme_biking/xtreme_biking/urls.py
+++ xtreme_biking/xtreme_biking/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from client import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
 ]
+
