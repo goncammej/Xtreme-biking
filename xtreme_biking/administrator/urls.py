1ac12722f55a91203ff7fa21b7810c4e550dab6d
alblopnov <a.lopezbenjumea@gmail.com>
2023-11-19 14:10:03 +0100

    Implementado la vista de incidencias de parte del admin

--- xtreme_biking/administrator/urls.py
+++ xtreme_biking/administrator/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('orders/', views.admin_orders_details, name='admin_orders'),
+    path('incidents/', views.admin_incidents_details, name='admin_incidents'),
 ]
--- xtreme_biking/administrator/views.py
+++ xtreme_biking/administrator/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404  
 
 # Create your views here.
 
 def admin_orders_details(request):
     return render(request, 'list_orders.html')
+
+def admin_incidents_details(request):
+    return render(request, 'list_incidents.html')
