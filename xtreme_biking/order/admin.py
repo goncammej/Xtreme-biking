434e773a428250d32930ccd3e23909ad2e067b2a
goncammej <goncammej@alum.us.es>
2023-12-07 15:32:20 +0100

    features

--- xtreme_biking/order/admin.py
+++ xtreme_biking/order/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from order.models import Order, OrderItem, ShippingAddress, CustomerReviews
+from order.models import Order, OrderItem, ShippingAddress, CustomerReviews, CustomerClaims
 
 # Register your models here.
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(ShippingAddress)
 admin.site.register(CustomerReviews)
+admin.site.register(CustomerClaims)
