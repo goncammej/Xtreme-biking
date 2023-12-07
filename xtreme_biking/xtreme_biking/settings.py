3f12c5e475a6031cdc31a90d23fe64f62f145e1b
goncammej <goncammej@alum.us.es>
2023-12-07 15:43:59 +0100

    features

--- xtreme_biking/xtreme_biking/settings.py
+++ xtreme_biking/xtreme_biking/settings.py
@@ -17,7 +17,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-BASEURL = 'https://test-xtreme-web.onrender.com'
+BASE_URL = 'https://test-xtreme-web.onrender.com'
 
 
 # Quick-start development settings - unsuitable for production
