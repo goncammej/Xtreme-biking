c5e9441be4e44023fe1e8400c5482c2b08f66d32
alejandromd <aleprieto02@gmail.com>
2023-12-12 20:16:05 +0100

    fix:actualizacion de settings

--- xtreme_biking/xtreme_biking/settings.py
+++ xtreme_biking/xtreme_biking/settings.py
@@ -156,3 +156,10 @@ LOGOUT_REDIRECT_URL = "/"
 CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
 
 CRISPY_TEMPLATE_PACK = "tailwind"
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "xtr3m3biking@gmail.com"
+EMAIL_HOST_PASSWORD = "jkar faaa plrk fawf"
